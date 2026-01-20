6e5327eb89c56ddf8d4087bb0189b889ed8b4785
yassmine244 <yasminebnr704@gmail.com>
2026-01-20 19:18:43 +0100

    fin

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
+// 1 
+function sommeDeN(n) {
+   var somme = 0;
+   var i = 1;
 
+   while (i <= n) {
+      somme = somme + i;
+      i++;
+   }
+
+   return somme;
+}
+
+// 2
+function factorielleDeN(n) {
+   var resultat= 1;
+   var i = 1;
+
+   while (i <= n) {
+      resultat = resultat * i;
+      i++;
+   }
+
+   return resultat;
+}
+
+// 3 
+function répéterChaîne(chaîne, nombre) {
+   var resultat = "";
+   var i = 0;
+
+   while (i < nombre) {
+      resultat = resultat + chaîne;
+      i++;
+   }
+
+   return resultat;
+}
+
+// 4 
+function sommeMinÀMax(minimum, maximum) {
+   var somme = 0;
+   var i = minimum;
+
+   while (i < maximum) {
+      somme = somme + i;
+      i++;
+   }
+
+   return somme;
+}
+
+// 5
+function produitMinÀMax(minimum, maximum) {
+   var produit = 1;
+   var i = minimum;
+
+   while (i < maximum) {
+      produit = produit * i;
+      i++;
+   }
+
+   return produit;
+}
